57e7376c63535942c2a6b475ced51a793df4f4b5
yoiner198 <conmuavi08@gmail.com>
2023-11-24 19:35:20 -0500

    nooone

---
+++ Config/Config.php
@@ -0,0 +1,16 @@
+<?php
+const BASE_URL = "http://localhost/tienda-online/";
+const HOST = "localhost";
+const USER = "root";
+const PASS = "";
+const DB = "tienda-online";
+const CHARSET = "charset=utf8";
+const TITLE = "TIENDA ONLINE";
+const MONEDA = "USD";
+const CLIENT_ID = "AWrxlVm3YzvgAxdjx2uux-xl20NqGupD6mJeo8keiRy-8cpDKBVZY1L66qNwjE7Zx-Vn25tUB47JjYIq";
+
+const USER_SMTP = "yoandering@gmail.com";
+const PASS_SMTP = "bhmbebsbcgzxogal";
+const PUERTO_SMTP = 465;
+const HOST_SMTP = "smtp.gmail.com";
+?>

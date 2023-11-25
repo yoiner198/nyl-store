5911fad3862d163732dec0a5f146888b4f4b6234
Yoandergl <117281785+Yoandergl@users.noreply.github.com>
2023-11-25 14:41:39 -0500

    2023

--- Config/Config.php
+++ Config/Config.php
@@ -9,8 +9,8 @@ const TITLE = "NYL STORE";
 const MONEDA = "USD";
 const CLIENT_ID = "AWrxlVm3YzvgAxdjx2uux-xl20NqGupD6mJeo8keiRy-8cpDKBVZY1L66qNwjE7Zx-Vn25tUB47JjYIq";
 
-const USER_SMTP = "tiendanylstoreg@gmail.com";
-const PASS_SMTP = "npjqnhosecwtuvcr";
+const USER_SMTP = "tiendanylstore@gmail.com";
+const PASS_SMTP = "cxhajnakidmtgtxh";
 const PUERTO_SMTP = 465;
 const HOST_SMTP = "smtp.gmail.com";
 ?>

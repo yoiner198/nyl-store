5a3be218ca2fd1d762c9448642e881da81130014
Yoandergl <117281785+Yoandergl@users.noreply.github.com>
2023-11-25 02:36:43 -0500

    1

--- Views/template/header-admin.php
+++ Views/template/header-admin.php
@@ -30,7 +30,7 @@
       <i class="fas fa-times p-3 cursor-pointer text-white opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
       <a class="navbar-brand m-0" href="<?php echo BASE_URL; ?>" target="_blank">
         <img src="<?php echo BASE_URL; ?>assets/images/logo.png" class="navbar-brand-img h-100" alt="main_logo">
-        <span class="ms-1 font-weight-bold text-white">Tienda</span>
+        <span class="ms-1 font-weight-bold text-white">NYL STORE</span>
       </a>
     </div>
     <hr class="horizontal light mt-0 mb-2">

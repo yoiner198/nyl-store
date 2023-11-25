11c7ea99c920069b72ba9c3adb411770bcaf89e0
yoiner198 <conmuavi08@gmail.com>
2023-11-25 04:46:44 -0500

    push

--- Views/admin/login.php
+++ Views/admin/login.php
@@ -26,7 +26,7 @@
           <div class="col-lg-4 col-md-8 col-12 mx-auto">
             <div class="card z-index-0 fadeIn3 fadeInBottom">
               <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2">
-                <div class="bg-gradient-primary shadow-primary border-radius-lg py-3 pe-1">
+                <div class="bg-gradient-dark shadow-primary border-radius-lg py-3 pe-1">
                   <h4 class="text-white font-weight-bolder text-center mt-2 mb-0">Sign in</h4>
                 </div>
               </div>
@@ -37,11 +37,11 @@
                     <input type="email" id="email" name="email" class="form-control">
                   </div>
                   <div class="input-group input-group-outline mb-3">
-                    <label class="form-label">Password</label>
+                    <label class="form-label">Contraseña</label>
                     <input type="password" id="clave" name="clave" class="form-control">
                   </div>
                   <div class="text-center">
-                    <button type="submit" class="btn bg-gradient-primary w-100 my-4 mb-2">Sign in</button>
+                    <button type="submit" class="btn bg-gradient-dark w-100 my-4 mb-2">Ingresar</button>
                   </div>
                 </form>
               </div>

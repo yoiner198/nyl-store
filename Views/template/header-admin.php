9bab8e40b19dcce6ab1ebede3835bc937b067277
Yoandergl <117281785+Yoandergl@users.noreply.github.com>
2023-11-25 02:39:43 -0500

    Q

--- Views/template/header-admin.php
+++ Views/template/header-admin.php
@@ -7,7 +7,7 @@
   <link rel="apple-touch-icon" sizes="76x76" href="<?php echo BASE_URL; ?>assets/images/logo.png">
   <link rel="icon" type="image/png" href="<?php echo BASE_URL; ?>assets/images/logo.png">
   <title>
-    Tienda | Online
+    NYL STORE
   </title>
   <!--     Fonts and icons     -->
   <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900|Roboto+Slab:400,700" />
